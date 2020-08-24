47df6caa7bccefcbc9f16dbc9727df56b9912d9e
Nirmali1234 <niniperera1234@gmail.com>
2020-08-24 10:05:01 +0530

    Added mongoose

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 6000;
 
 app.use(express.json());
+
+mongoose //database connection string
+  .connect("mongodb://localhost/itemdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Db successfully..."))
+  .catch((err) =>
+    console.log("Error has occured while connecting to db : ", err)
+  );
 
 app.listen(PORT, function () {
     console.log("Listening on port - " + PORT);
