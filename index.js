e1b774edb4a0eebf6cee233eff2ee6205236da48
Nirmali1234 <niniperera1234@gmail.com>
2020-08-24 10:02:15 +0530

    Added mongoose

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = 6000;
+
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log("Listening on port - " + PORT);
+  });
