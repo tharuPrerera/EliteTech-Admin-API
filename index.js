9386f22e5b37922d7e29ed6c0bb28ae1cf51fa9d
Nirmali1234 <niniperera1234@gmail.com>
2020-08-26 17:54:01 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const products = require('./routes/products');
 const app = express();
 const PORT = 6000;
 
+app.use(cors());
 app.use(express.json());
+app.use('/api/products',products);
 
 mongoose //database connection string
   .connect("mongodb://localhost/itemdb", {
