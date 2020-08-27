32f0a89b8133da978be974bbcfa0431b3a6b62bd
Nirmali1234 <niniperera1234@gmail.com>
2020-08-27 12:31:23 +0530

    Updated products route

--- routes/products.js
+++ routes/products.js
@@ -19,13 +19,18 @@ router.get("/:productId", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  if (!req.body.productName) {
+  if (!req.body.itemName) {
     return res.status(400).send("Not all mandetory values have been set");
   }
   try {
     let productToBeAddedToDb = new Product({
-      itemName: req.body.productName,
-      itemPrice: req.body.itemPrice,
+      imgUrl:req.body.imgUrl,
+      itemName: req.body.itemName,
+      unitPrice: req.body.unitPrice,
+      brand:req.body.brand,
+      code:req.body.code,
+      warranty:req.body.warranty,
+      quantity:req.body.quantity,
     });
 
     productToBeAddedToDb = await productToBeAddedToDb.save();
@@ -38,7 +43,15 @@ router.post("/", async (req, res) => {
 router.put("/:productId", async (req, res) => {
   let product = await Product.findOneAndUpdate(
     { _id: req.params.productId },
-    { $set: { itemPrice: req.body.itemPrice } },
+    { $set: { 
+      imgUrl:req.body.imgUrl,
+      itemName: req.body.itemName,
+      unitPrice: req.body.unitPrice,
+      brand:req.body.brand,
+      code:req.body.code,
+      warranty:req.body.warranty,
+      quantity:req.body.quantity
+             } },
     { new: true, useFindAndModify: false }
   );
   res.send(product);
