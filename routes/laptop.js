b4f85f5b8afa1627bd96955e33f524241e1a875f
Nirmali1234 <niniperera1234@gmail.com>
2020-08-30 12:42:07 +0530

    Updated routes

---
+++ routes/laptop.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const laptopItem = require("../models/laptopItem");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  let laptop = await laptopItem.find();
+  res.send(laptop);
+});
+
+router.get("/:productId", async (req, res) => {
+  let product = await laptopItem.findById(req.params.productId);
+  if (!product) {
+    return res
+      .sendStatus(400)
+      .send("The given id does not exist on our server...");
+  }
+
+  res.send(product);
+});
+
+router.post("/", async (req, res) => {
+  if (!req.body.itemName) {
+    return res.status(400).send("Not all mandetory values have been set");
+  }
+  try {
+    let productToBeAddedToDb = new laptopItem({
+      imgUrl:req.body.imgUrl,
+      itemName: req.body.itemName,
+      unitPrice: req.body.unitPrice,
+      brand:req.body.brand,
+      code:req.body.code,
+      warranty:req.body.warranty,
+      quantity:req.body.quantity,
+    });
+
+    productToBeAddedToDb = await productToBeAddedToDb.save();
+    res.send(productToBeAddedToDb);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+});
+
+router.put("/:productId", async (req, res) => {
+  let product = await laptopItem.findOneAndUpdate(
+    { _id: req.params.productId },
+    { $set: { 
+      imgUrl:req.body.imgUrl,
+      itemName: req.body.itemName,
+      unitPrice: req.body.unitPrice,
+      brand:req.body.brand,
+      code:req.body.code,
+      warranty:req.body.warranty,
+      quantity:req.body.quantity
+             } },
+    { new: true, useFindAndModify: false }
+  );
+  res.send(product);
+});
+
+router.delete("/:productId", async (req, res) => {
+  let product = await laptopItem.findOneAndDelete({ _id: req.params.productId });
+
+  if (!product) {
+    return res.status(404).send("Product Id does not exit");
+  }
+  res.send({product:"Record has been Deleted..!!"});
+});
+
+module.exports = router;
