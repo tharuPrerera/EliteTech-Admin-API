d65d78ba2006807cbd99b0fabfdd6834f6c5e723
Nirmali1234 <niniperera1234@gmail.com>
2020-09-05 00:13:37 +0530

    Updated model

--- models/accessoriesItem.js
+++ models/accessoriesItem.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const accessoriesItemSchema = new mongoose.Schema({
       imgUrl: {
         type: String,
-        required: true,
       },
       itemName: {
         type: String,
